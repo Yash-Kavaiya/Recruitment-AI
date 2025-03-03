6696eae7022622f8734f07eb60a43116bc341944
Yash Kavaiya <yash.kavaiya3@gmail.com>
2025-03-03 06:28:23 +0000

    update Next.js configuration to include outputFileTracingRoot and improve experimental settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const path = require('path');
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	outputFileTracingRoot: path.join(__dirname, '../../'),
+	experimental: {
+		// ...existing experimental configuration...
+	},
+};
 
-
-
-
-
-
-
-module.exports = nextConfig}  },    outputFileTracingRoot: path.join(__dirname, '../../'),  experimental: {const nextConfig = {/** @type {import('next').NextConfig} */
+module.exports = nextConfig;
